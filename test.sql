d900bcec713f957ea017fee62cea7be1f79e509e
DarbanMM <darbanmaha@gmail.com>
2025-06-05 07:51:42 +0700

    third commit

--- test.sql
+++ test.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2025 at 01:01 PM
--- Server version: 11.5.2-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 05 Jun 2025 pada 02.49
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Struktur dari tabel `login`
 --
 
 CREATE TABLE `login` (
@@ -36,7 +36,7 @@ CREATE TABLE `login` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `login`
+-- Dumping data untuk tabel `login`
 --
 
 INSERT INTO `login` (`id`, `username`, `password`, `nama`, `level`) VALUES
@@ -44,12 +44,15 @@ INSERT INTO `login` (`id`, `username`, `password`, `nama`, `level`) VALUES
 (30, '22106050046', 'yusrina', 'YUSRINA MASTURA', 'admin'),
 (31, '22106050083', 'darban', 'DARBAN MAHA MURSYIDI', 'admin'),
 (32, '22106050085', 'akrimna', 'AKRIMNA FAHMA', 'admin'),
-(34, 'mahasiswa', 'mahasiswa', 'Mahasiswa', 'mahasiswa');
+(34, 'mahasiswa', 'mahasiswa', 'Mahasiswa', 'mahasiswa'),
+(63, '22106050046', '123456', 'YUSRINA MASTURA', 'mahasiswa'),
+(64, '22106050083', '123456', 'DARBAN MAHA MURSYIDI', 'mahasiswa'),
+(65, '22106050085', '123456', 'AKRIMNA FAHMA', 'mahasiswa');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `register`
+-- Struktur dari tabel `register`
 --
 
 CREATE TABLE `register` (
@@ -63,7 +66,7 @@ CREATE TABLE `register` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reservasi`
+-- Struktur dari tabel `reservasi`
 --
 
 CREATE TABLE `reservasi` (
@@ -79,63 +82,55 @@ CREATE TABLE `reservasi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `reservasi`
+-- Dumping data untuk tabel `reservasi`
 --
 
 INSERT INTO `reservasi` (`id`, `nama`, `asal`, `fakultas`, `keperluan`, `ruang`, `tanggal`, `waktu`, `waktu_selesai`) VALUES
-(9, 'Darban Maha Mursyidi', 'HMPS', 'Adab dan Ilmu Budaya', 'Rapat Bulanan', 'Co-Working Space A', '2025-01-02', '19:05:00', '00:00:00'),
-(10, 'Fakhri Arrouf', 'HMPS', 'Adab dan Ilmu Budaya', 'Mungkid', 'Co-Working Space A', '2024-12-02', '04:02:00', '06:06:00'),
-(11, 'Darban Maha Mursyidi', 'Umum', 'Sains dan Teknologi', 'Belajar kelompok', 'Co-Working Space D', '2024-12-02', '11:16:00', '18:16:00'),
-(14, 'YUSRINA MASTURA', 'Umum', 'Sains dan Teknologi', 'Belajar kelompok', 'Co-Working Space Timur', '2024-12-25', '00:00:00', '20:10:00'),
-(16, 'Fakhri Arrouf', 'HMPS', 'Ushuluddin dan Pemikiran Islam', 'Rapat Bulanan', 'Co-Working Space C', '2024-12-25', '08:00:00', '10:45:00'),
-(17, 'AKRIMNA FAHMA', 'iNFORMATIKA', 'Sains dan Teknologi', 'Pembahasan sportif', 'Co-Working Space C', '2024-12-25', '00:00:00', '13:00:00'),
-(18, 'AKRIMNA FAHMA', 'INFORMATIKA', 'Sains dan Teknologi', 'Pengajuan Pemberian Penghargaan Prestasi Mahasiswa UIN Sunan Kalijaga 2024', 'Co-Working Space E', '2024-12-23', '15:00:00', '18:00:00'),
-(19, 'Darban Maha Mursyidi', 'Umum', 'Sains dan Teknologi', 'Belajar kelompok', 'Co-Working Space F', '2024-12-31', '15:27:00', '19:30:00'),
-(20, 'Darban Maha Mursyidi', 'HMPS', 'Ilmu Sosial dan Humaniora', 'Rapat Bulanan', 'Co-Working Space Timur', '2024-12-28', '15:50:00', '23:50:00');
+(22, 'Mahasiswa', 'Umum', 'Adab dan Ilmu Budaya', 'Belajar kelompok', 'Co-Working Space A', '2025-01-02', '18:20:00', '19:18:00');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `login`
+-- Indeks untuk tabel `login`
 --
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `register`
+-- Indeks untuk tabel `register`
 --
 ALTER TABLE `register`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `reservasi`
+-- Indeks untuk tabel `reservasi`
 --
 ALTER TABLE `reservasi`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `login`
+-- AUTO_INCREMENT untuk tabel `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
--- AUTO_INCREMENT for table `register`
+-- AUTO_INCREMENT untuk tabel `register`
 --
 ALTER TABLE `register`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `reservasi`
+-- AUTO_INCREMENT untuk tabel `reservasi`
 --
 ALTER TABLE `reservasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
